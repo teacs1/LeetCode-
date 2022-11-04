eb4642ea751408c00c5a0fe45de1bf8c48979ce3
Proprobe <proprobe@foxmail.com>
2022-11-04 14:39:11 +0800

    day1 problem(1,2,3,4)

--- src/main/java/com/leetcode/config/SpringConfig.java
+++ src/main/java/com/leetcode/config/SpringConfig.java
@@ -2,8 +2,6 @@ package com.leetcode.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
 @Configuration  //设置为配置类
 @ComponentScan({"com.leetcode.hot100"})  //用于加载控制的Bean
 //@PropertySource("classpath:jdbc.properties")  //加载properties文件
